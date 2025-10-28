1149d4de179fc6473c68c18ac8ff01829d803cab
user <agnessaayrapetova@gmail.com>
2025-10-28 22:33:07 +0300

    fix lab03(add comments)

--- src/lab03/text_stats.py
+++ src/lab03/text_stats.py
@@ -9,10 +9,13 @@ print(f'Всего слов: {len(data)}\nУникальных слов: {len(se
 f=False
 if f:
     longest_word = len(max(count_freq(data), key=len))+5
-    print(f'слово{(longest_word-5) * ' '}| частота')
+    print(f'слово{(longest_word-5) * " "}| частота')
     print((longest_word + 9) * '-')
     print('\n'.join([word[0] + ' ' * (longest_word-len(word[0])) + '| ' + str(word[1]) for word in top_n(count_freq(data))]))
 else:
-    print(f'Топ-5:\n{'\n'.join((f'{i[0]}:{i[1]}' for i in top_n(count_freq(data))))}')
+    top_words = top_n(count_freq(data))
+    print("Топ-5:")
+    for word, count in top_words:
+        print(f"{word}: {count}")
 
 #hello world hello python test world hello
