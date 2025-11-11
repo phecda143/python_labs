351732240a9db3b72269b64e640f553dd20cd4a9
user <agnessaayrapetova@gmail.com>
2025-11-11 19:24:44 +0300

    добавила 2 задание 5 лабы и поправила структуру проекта

--- src/lab03/text_stats.py
+++ src/lab03/text_stats.py
@@ -1,21 +1,27 @@
 import sys
-'''добавляем нужный нам путь в список путей, где Python ищет модули при импорте'''
-sys.path.append('C:/Users/matve/PycharmProjects/python_labs/src/lib')
-'''импортируем созданные ранее функции'''
-from src.lib.moduls import normalize, tokenize, count_freq, top_n
-data = sys.stdin.read()
-data = [i.casefold() for i in tokenize(normalize(data))]
-print(f'Всего слов: {len(data)}\nУникальных слов: {len(set(data))}')
-f=False
-if f:
-    longest_word = len(max(count_freq(data), key=len))+5
-    print(f'слово{(longest_word-5) * " "}| частота')
-    print((longest_word + 9) * '-')
-    print('\n'.join([word[0] + ' ' * (longest_word-len(word[0])) + '| ' + str(word[1]) for word in top_n(count_freq(data))]))
-else:
-    top_words = top_n(count_freq(data))
-    print("Топ-5:")
-    for word, count in top_words:
-        print(f"{word}: {count}")
 
-#hello world hello python test world hello
+
+def main():
+    '''добавляем нужный нам путь в список путей, где Python ищет модули при импорте'''
+    '''импортируем созданные ранее функции'''
+    from src.lib.moduls import normalize, tokenize, count_freq, top_n
+    data = sys.stdin.read()
+    data = [i.casefold() for i in tokenize(normalize(data))]
+    print(f'Всего слов: {len(data)}\nУникальных слов: {len(set(data))}')
+    f = False
+    if f:
+        longest_word = len(max(count_freq(data), key=len)) + 5
+        print(f'слово{(longest_word - 5) * " "}| частота')
+        print((longest_word + 9) * '-')
+        print('\n'.join(
+            [word[0] + ' ' * (longest_word - len(word[0])) + '| ' + str(word[1]) for word in top_n(count_freq(data))]))
+    else:
+        top_words = top_n(count_freq(data))
+        print("Топ-5:")
+        for word, count in top_words:
+            print(f"{word}: {count}")
+
+
+if __name__ == '__main__':
+    main()
+# hello world hello python test world hello
---
+++ src/lib/csv_json.py
@@ -0,0 +1,49 @@
+import json
+import csv
+
+
+def json_to_csv(json_path: str, csv_path: str) -> None:
+    """
+    Преобразует JSON-файл в CSV.
+    Поддерживает список словарей [{...}, {...}], заполняет отсутствующие поля пустыми строками.
+    Кодировка UTF-8. Порядок колонок — как в первом объекте или алфавитный (указать в README).
+    """
+    if not (json_path.endswith('.json')) or not (csv_path.endswith('.csv')):
+        raise TypeError('Неправильный формат файла')
+    try:
+        with open(json_path, 'r', encoding='utf-8') as jf:
+            file = json.load(jf) # возвращает список словарей
+        if not isinstance(file, list):
+            raise ValueError("JSON должен содержать список объектов")
+        if len(file) == 0:
+            raise ValueError("Пустой JSON или неподдерживаемая структура")
+        if not isinstance(file[0], dict):
+            raise ValueError("Элементы списка должны быть словарями")
+
+        with open(csv_path, 'w', encoding='utf-8') as cf:
+            cf = csv.DictWriter(cf, fieldnames=list(file[0].keys()))
+            cf.writeheader()
+            cf.writerows(file)
+
+    except FileNotFoundError:
+        raise FileNotFoundError("Файл не найден")
+
+
+def csv_to_json(csv_path: str, json_path: str) -> None:
+    """
+    Преобразует CSV в JSON (список словарей).
+    Заголовок обязателен, значения сохраняются как строки.
+    json.dump(..., ensure_ascii=False, indent=2)
+    """
+    if not (json_path.endswith('.json')) or not (csv_path.endswith('.csv')):
+        raise TypeError('Неправильный формат файла')
+    try:
+        with open(csv_path, 'r', encoding='utf-8') as cf:
+            file = list(csv.DictReader(cf))
+        if len(file) == 0:
+            raise ValueError('CSV файл пуст')
+
+        with open(json_path, 'w', encoding='utf-8') as jf:
+            json.dump(file, jf, ensure_ascii=False, indent=2)
+    except FileNotFoundError:
+        raise FileNotFoundError("Файл не найден")
